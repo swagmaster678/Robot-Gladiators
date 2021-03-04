ed45cb56bb5e3d49d760a087e32f6369777371c4
Brice Whitefield <bricewhitefield@yahoo.com>
2021-03-03 18:33:04 -0600

    initial game setup

--- game.js
+++ game.js
@@ -10,16 +10,14 @@ console.log(enemyName, enemyAttack, enemyHealth);
 
 var fight = function() {
     alert ("Welcome to Robot Gladiators");
-//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 
-  // Log a resulting message to the console so we know that it worked.
-
-  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
 playerHealth = playerHealth = enemyAttack;
-  // Log a resulting message to the console so we know that it worked.
+
 console.log(
   enemyName + " attacked " + playerName + " , " + playerName + " now has " + playerHealth + " health reamining. ")
 };
+//checking players health
 if (playerHealth <= 0) {
   alert (playerName + " has died!");
 }
@@ -29,17 +27,29 @@ else {
 
 fight();
 
+var promptFight = window.prompt ("would you like to 'FIGHT' or 'SKIP' to choose.")
+
+//prompts user to fight, pressing confirm will make them fight.
+if (promptFight === "fight" || promptFight === "FIGHT") { 
+
 //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 enemyHealth = enemyHealth - playerAttack;
 
-// Log a resulting message to the console so we know that it worked.
 console.log(
   playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
 );
-
+//checking enemy's health
 if (enemyHealth <= 0) {
   alert (enemyName = " has died!");
 }
 else {
   alert (enemyName + " still has " + enemyHealth + " health left.");
+}
+}
+// if player choses to skip 
+else if (promptFight === "skip" || promptFight === "SKIP") {
+  alert(playerName + " has chosen to skip the fight!");
+}
+else { 
+  alert("You need to choose a valid option. Try again!")
 }
